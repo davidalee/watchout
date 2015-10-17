1a39aede5830011fffec1a6e5a6539beec5552a8
David Lee <david7lee@gmail.com>
2015-10-17 14:55:05 -0400

    players and enemies appended to DOM

--- client/watchout.js
+++ client/watchout.js
@@ -20,9 +20,72 @@ var axes = {
 var board = d3.select('#gameboard').append('svg').attr('width', options.width).attr('height', options.height);
 
 // create player object
+var player = {
+  position: {
+    'x': 400,
+    'y': 250
+  }
+
   // changing player object position based on drag input
+
+};
 // create Enemy class
+var Enemy = function(position) {
+  this.position = position || randomCoordinateGenerator();
+
+  // consider shape property
+
   // random movement within board
+
+};
+
+Enemy.prototype.move = function() {
+  // update this.position with a random pair of coords
+  this.position = randomCoordinateGenerator();
+};
+
+function randomCoordinateGenerator() {
+  var x = Math.floor(Math.random() * options.width);
+  var y = Math.floor(Math.random() * options.height);
+  //return two random coordinates (on the board area)
+  return {x: x, y: y};
+};
+
+// Create player and append to DOM
+var playerArr = [player];
+var playerInstance = board.selectAll('circle').data(playerArr).enter().append('circle')
+                          .attr('cx', player.position.x).attr('cy', player.position.y).attr('r', 10);
+var drag = d3.behavior.drag().on('drag', function(playerOb) {
+  var newX = + playerInstance.attr('cx') + d3.event.dx;
+  var newY = + playerInstance.attr('cy') + d3.event.dy;
+  playerInstance.attr('cx', newX);
+  playerInstance.attr('cy', newY);
+}).origin(function() {
+  return player.position;
+});
+playerInstance.call(drag);
+
+// Create and instantiate Enemies, append to DOM
+var enemiesArr = [];
+for( var i = 0; i < options.enemyCount; i++ ){
+  enemiesArr.push(new Enemy);
+}
+var enemyInstances = board.selectAll('circle').data(enemiesArr).enter().append('circle').attr('cx', function(d){
+  return d.position.x;
+}).attr('cy', function(d){
+  return d.position.y;
+}).attr('r', 10).attr('fill', 'red');
+
+
+// every tick: use d3.timer(xMilliSec, cb) to call the transition function(takes a cb [randomMove]) to animate
+// use the transition function with tween to call collision detection function
+
+
+
+
+
+
+
 // collision detection
   // detect when a player collides with an enemy
   // list coordinates that define the area of each enemy
@@ -35,5 +98,17 @@ var board = d3.select('#gameboard').append('svg').attr('width', options.width).a
     // every tick: check for collisions
     // every tick: update score (if needed)
 
-    // every tick: use d3.timer(xMilliSec, cb) to call the transition function(takes a cb [randomMove]) to animate
-    // use the transition function with tween to call collision detection function
+
+// maybe use General Update pattern style to generate enemies and append to DOM (game board?)
+
+
+
+
+
+// ****** Cool ideas ******
+// having an enemy that moves towards the player (using .force, .gravity)
+// point threshold tiers?
+// implement difficulty tiers (e.g., hard mode, nightmare mode) that can be toggled on/off?
+  // change enemy to somethng else for each level (whirling shuriken)
+  // create a unique enemy for nightmare mode (the enemy that gravitates toward player?); "boss"
+// reverse the game objective so instead of avoiding enemies, player tries to catch enemies, implement points multiplier?
